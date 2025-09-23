29b6e6dd62ec4f0480d6d27254c2034f71f84290
Shuo Chen <chen@mitre.org>
2025-09-23 16:14:25 -0400

    Adding chroma database interface and population.

--- alue/rag_utils.py
+++ alue/rag_utils.py
@@ -2,8 +2,17 @@ import json
 import logging
 import os
 from pprint import pformat
-from typing import Optional, List, Dict, Any
+from typing import Optional, List, Dict, Any, Tuple
 
+from chromadb import (
+    Collection,
+    EmbeddingFunction,
+    Metadata,
+    PersistentClient,
+)
+from chromadb.api import ClientAPI
+from chromadb.api.types import Embeddable
+from chromadb.utils import embedding_functions
 from unstructured.chunking.basic import chunk_elements
 from unstructured.chunking.title import chunk_by_title
 from unstructured.documents.elements import (
@@ -30,8 +39,8 @@ def setup_logger(name: str) -> logging.Logger:
     logger.addHandler(console_handler)
     return logger
 
-
 logger = setup_logger(__name__)
+
 
 class DocumentProcessor:
 
@@ -57,21 +66,27 @@ class DocumentProcessor:
     COMBINE_TEXT_UNDER_N_CHARS = 500
     MULTIPAGE_SECTIONS = True
 
-    document_directory_path: str
+    document_directory_path: Optional[str]
     output_path: str
+    temp_document_path: Optional[str]
+    partition_strategy: str 
+    hi_res_model_name: str 
+    extracted_image_block_types: Optional[List[str]] 
+    write_image_block_types_metadata: Optional[bool] 
+    elements_to_keep: List[str] 
+    chunk_hard_max_chars: int
+    chunk_soft_max_chars: int
+    overlap_size: int 
+    combine_text_under_n_chars: int
+    multipage_sections: bool 
+    chunk_type: str 
+    write_to_file: bool
 
 
     def __init__(
         self,
-        document_directory_path: str,
-        output_path: str
-    ):
-        self.document_directory_path = document_directory_path
-        self.output_path = output_path
-
-
-    def process_document_directory(
-        self,
+        document_directory_path: Optional[str],
+        output_path: str,
         temp_document_path: Optional[str] = None,
         partition_strategy: str = "hi_res",
         hi_res_model_name: str = "yolox",
@@ -85,35 +100,45 @@ class DocumentProcessor:
         multipage_sections: bool = MULTIPAGE_SECTIONS,
         chunk_type: str = "title",
         write_to_file: bool = True,
+    ):
+        self.document_directory_path = document_directory_path
+        self.output_path = output_path
+        self.temp_document_path = temp_document_path
+        self.partition_strategy = partition_strategy
+        self.hi_res_model_name = hi_res_model_name
+        self.extracted_image_block_types = extracted_image_block_types 
+        self.write_image_block_types_metadata = write_image_block_types_metadata
+        self.elements_to_keep = elements_to_keep 
+        self.chunk_hard_max_chars = chunk_hard_max_chars
+        self.chunk_soft_max_chars = chunk_soft_max_chars
+        self.overlap_size = overlap_size
+        self.combine_text_under_n_chars = combine_text_under_n_chars
+        self.multipage_sections = multipage_sections
+        self.chunk_type = chunk_type
+        self.write_to_file = write_to_file
+
+
+    def process_document_directory(
+        self
     ) -> List[Dict[str,Any]]:
         
         chunks = []
-        for dirpath, _, filenames in os.walk(self.document_directory_path):
-            logger.info(f"Processing documents in {dirpath}")
+        if self.document_directory_path:
+            for dirpath, _, filenames in os.walk(self.document_directory_path):
+                logger.info(f"Processing documents in {dirpath}")
 
-            for filename in filenames:
-                document_path = os.path.join(dirpath, filename)
-                logger.info(f"Processing document {document_path}")
-                doc_chunks = self.process_single_document(
-                    document_path,
-                    temp_document_path,
-                    partition_strategy,
-                    hi_res_model_name,
-                    extracted_image_block_types,
-                    write_image_block_types_metadata,
-                    elements_to_keep,
-                    chunk_hard_max_chars,
-                    chunk_soft_max_chars,
-                    overlap_size,
-                    combine_text_under_n_chars,
-                    multipage_sections,
-                    chunk_type,
-                    write_to_file,
-                )
-                logger.info(f"Retrieved {len(doc_chunks)} chunks from {document_path}")
-                chunks.extend(doc_chunks)
+                for filename in filenames:
+                    if filename.lower().endswith("pdf"):
+                        document_path = os.path.join(dirpath, filename)
+                        logger.info(f"Processing document {document_path}")
+                        doc_chunks = self.process_single_document(document_path)
+                        logger.info(f"Retrieved {len(doc_chunks)} chunks from {document_path}")
+                        chunks.extend(doc_chunks)
 
-        logger.info(f"Returning {len(chunks)} chunks from documents in {self.document_directory_path}")
+            logger.info(f"Returning {len(chunks)} chunks from documents in {self.document_directory_path}")
+        else: 
+            logger.info(f"Paramter 'document_directory_path' must be set to generate document chunks. Returning empty chunks list.")
+
         return chunks
         
 
@@ -140,20 +165,7 @@ class DocumentProcessor:
 
     def process_single_document(
         self,
-        document_path: str,
-        temp_document_path: Optional[str] = None,
-        partition_strategy: str = "hi_res",
-        hi_res_model_name: str = "yolox",
-        extracted_image_block_types: Optional[List[str]] = EXTRACT_IMAGE_BLOCK_TYPES,
-        write_image_block_types_metadata: Optional[bool] = False,
-        elements_to_keep: List[str] = ["NarrativeText", "ListItem", "Title"],
-        chunk_hard_max_chars: int = CHUNK_HARD_MAX_CHARS,
-        chunk_soft_max_chars: int = CHUNK_SOFT_MAX_CHARS,
-        overlap_size: int = OVERLAP_SIZE,
-        combine_text_under_n_chars: int = COMBINE_TEXT_UNDER_N_CHARS,
-        multipage_sections: bool = MULTIPAGE_SECTIONS,
-        chunk_type: str = "title",
-        write_to_file: bool = True,
+        document_path: str
     ) -> List[Dict[str,Any]]:
         
         document_name = os.path.splitext(os.path.basename(document_path))[0]
@@ -169,38 +181,38 @@ class DocumentProcessor:
         logger.info(f"Partitioning pdf for {document_name}")
         elements = partition_pdf(
             filename=document_path,
-            strategy=partition_strategy,
+            strategy=self.partition_strategy,
             languages=["eng"],
             infer_table_structure=False,
-            extract_image_block_types=extracted_image_block_types,
+            extract_image_block_types=self.extracted_image_block_types,
             extract_image_block_output_dir=extracted_images_dir,
-            hi_res_model_name=hi_res_model_name,
+            hi_res_model_name=self.hi_res_model_name,
         )
 
         # write image metadata to document metadata file
-        if write_image_block_types_metadata:
+        if self.write_image_block_types_metadata:
             metadata_fpath = os.path.join(
                 artifacts_dir, "metadata_for_extracted_images.txt"
             )
             self.write_image_block_metadata(
-                elements, metadata_fpath, document_path, extracted_image_block_types
+                elements, metadata_fpath, document_path, self.extracted_image_block_types
             )
 
         # filter file elements before chunking, only keeping specific element types
         clean_elements = []
         for element in elements:
-            if element.category in elements_to_keep:
+            if element.category in self.elements_to_keep:
                 clean_elements.append(element)
 
         # specify arguments for chunking then pivot on chunking type
         basic_kwargs = {
-            "max_characters": chunk_hard_max_chars,
-            "new_after_n_chars": chunk_soft_max_chars,
-            "overlap": overlap_size,
+            "max_characters": self.chunk_hard_max_chars,
+            "new_after_n_chars": self.chunk_soft_max_chars,
+            "overlap": self.overlap_size,
         }
         title_kwargs = {
-            "combine_text_under_n_chars": combine_text_under_n_chars,
-            "multipage_sections": multipage_sections,
+            "combine_text_under_n_chars": self.combine_text_under_n_chars,
+            "multipage_sections": self.multipage_sections,
             **basic_kwargs,
         }
         chunks_basic_template = "type_{chunk_type}_soft_lim_{soft_max}_hard_lim_{hard_max}_overlap_{overlap_size}.txt"
@@ -211,18 +223,18 @@ class DocumentProcessor:
         logger.info(f"Chunking pdf elements for {document_name}")
         
         # create chunks based on type of chunking specified
-        if chunk_type == "title":
+        if self.chunk_type == "title":
             # chunk by title
             chunks = chunk_by_title(clean_elements, **title_kwargs)
-            if write_to_file:
+            if self.write_to_file:
                 # create the filenames and export chunks
                 chunks_title_fname = chunks_title_template.format(
-                    chunk_type=chunk_type,
-                    soft_max=chunk_soft_max_chars,
-                    hard_max=chunk_hard_max_chars,
-                    overlap_size=overlap_size,
-                    combine_text_under_n=combine_text_under_n_chars,
-                    multipage_ok=multipage_sections,
+                    chunk_type=self.chunk_type,
+                    soft_max=self.chunk_soft_max_chars,
+                    hard_max=self.chunk_hard_max_chars,
+                    overlap_size=self.overlap_size,
+                    combine_text_under_n=self.combine_text_under_n_chars,
+                    multipage_ok=self.multipage_sections,
                 )
                 # write chunks to text
                 self.save_chunks_to_txt(
@@ -237,16 +249,16 @@ class DocumentProcessor:
                     doc_path=document_path,
                 )
 
-        elif chunk_type == "basic":
+        elif self.chunk_type == "basic":
             # create chunks
             chunks = chunk_elements(clean_elements, **basic_kwargs)
-            if write_to_file:
+            if self.write_to_file:
                 # create the filenames and export chunks
                 chunks_basic_fname = chunks_basic_template.format(
-                    chunk_type=chunk_type,
-                    soft_max=chunk_soft_max_chars,
-                    hard_max=chunk_hard_max_chars,
-                    overlap_size=overlap_size,
+                    chunk_type=self.chunk_type,
+                    soft_max=self.chunk_soft_max_chars,
+                    hard_max=self.chunk_hard_max_chars,
+                    overlap_size=self.overlap_size,
                 )
                 # write chunks to text
                 self.save_chunks_to_txt(
@@ -267,6 +279,22 @@ class DocumentProcessor:
         output_chunks = self.to_json(chunks, document_name, document_path)
 
         return output_chunks
+
+
+    def save_chunks_to_txt(
+        self,
+        chunks: List["Element"], 
+        fname: str, 
+        identifier: str
+    ) -> None:
+        logger.info(f"Saving chunks to {fname}")
+        with open(fname, "w", encoding="utf-8") as f:
+            f.write(f"Identifier: {identifier}\n\n")
+            for chunk in chunks:
+                f.write(f"{str(chunk)}\n\n")
+                f.write(
+                    "----------------------------------------------------------------------------------------------------\n"
+                )
     
 
     def prepare_chunk_metadata(
@@ -284,22 +312,6 @@ class DocumentProcessor:
         }
         return metadata
 
-
-    def save_chunks_to_txt(
-        self,
-        chunks: List["Element"], 
-        fname: str, 
-        identifier: str
-    ) -> None:
-        logger.info(f"Saving chunks to {fname}")
-        with open(fname, "w", encoding="utf-8") as f:
-            f.write(f"Identifier: {identifier}\n\n")
-            for chunk in chunks:
-                f.write(f"{str(chunk)}\n\n")
-                f.write(
-                    "----------------------------------------------------------------------------------------------------\n"
-                )
-
     
     def to_json(
         self,
@@ -307,6 +319,7 @@ class DocumentProcessor:
         identifier: str,
         doc_path: Optional[str] = None,
     ) -> List[Dict[str,Any]]:
+        
         file_info = {}
         if doc_path is not None:
             file_dir, file_name = os.path.split(doc_path)
@@ -318,10 +331,12 @@ class DocumentProcessor:
                 "chunk_metadata": chunk.metadata,
                 "identifier": f"{identifier}-{i}",
             }
-            if file_info:
-                metadata_inputs.update(file_info)
+            metadata_inputs.update(file_info)
             chunk_metadata = self.prepare_chunk_metadata(**metadata_inputs)
-            chunk_dict = {"text": chunk.text, "metadata": chunk_metadata}
+            chunk_dict = {
+                "text": chunk.text, 
+                "metadata": chunk_metadata
+            }
             output.append(chunk_dict)
         
         return output
@@ -338,3 +353,139 @@ class DocumentProcessor:
         logger.info(f"Saving chunks to {fname}")
         with open(fname, "w", encoding="utf-8") as f:
             json.dump(output_chunks, f, indent=4)
+
+
+    @staticmethod
+    def load_chunks_from_json(
+        file_path: str,
+    ) -> List[Dict[str,Any]]:
+        chunks = []
+        with open(file_path, "r", encoding="utf-8") as f:
+            chunks.extend(json.load(f))
+        return chunks
+
+
+class ChromaInterface:
+
+    client: Optional[ClientAPI]  
+    database_path: str
+
+
+    def __init__(
+        self,
+        database_path: str,
+    ):
+        self.client = None
+        self.database_path = database_path
+
+
+    @staticmethod
+    def get_openai_embedding_function(model="text-embedding-3-small"):
+        return embedding_functions.OpenAIEmbeddingFunction(
+            api_key=os.getenv("OPENAI_API_KEY"),
+            model_name=model
+        )
+
+
+    @staticmethod
+    def get_ollama_embedding_function(model="nomic-embed-text", url="http://localhost:11434"):
+        return embedding_functions.OllamaEmbeddingFunction(
+            model_name=model,
+            url=f"{url}/api/embeddings"
+        )
+    
+
+    @staticmethod
+    def get_hf_embedding_function(model="sentence-transformers/all-MiniLM-L6-v2"):
+        return embedding_functions.HuggingFaceEmbeddingFunction(
+            api_key=os.getenv("HF_TOKEN"),
+            model_name=model
+        )
+
+
+    @staticmethod
+    def get_local_embedding_function(model="all-MiniLM-L6-v2"):
+        return embedding_functions.SentenceTransformerEmbeddingFunction(
+            model_name=model
+        )
+
+
+    @staticmethod
+    def get_openai_compatible_embedding_function(base_url, model="default", api_key="dummy"):
+        return embedding_functions.OpenAIEmbeddingFunction(
+            api_key=api_key,
+            model_name=model,
+            api_base=base_url
+        )
+
+
+    def load_or_create_db(
+        self,
+    ) -> None:
+        if self.client is None and self.database_path:
+            self.client = PersistentClient(path=self.database_path)
+
+
+    def get_or_create_collection(
+        self,
+        collection_name: str,
+        embedding_function: Optional[EmbeddingFunction[Embeddable]] = None,
+    ) -> Collection:
+        # create client if it does not exist
+        if self.client is None:
+            self.load_or_create_db()
+
+        if embedding_function is None:
+            embedding_function = ChromaInterface.get_local_embedding_function()
+        
+        return self.client.get_or_create_collection(
+            name=collection_name,
+            embedding_function=embedding_function
+        )
+
+
+    def get_collection(
+        self,
+        collection_name: str,
+    ) -> Optional[Collection]:
+        # create client if it does not exist
+        if self.client is None:
+            self.load_or_create_db()
+
+        return self.client.get_or_create_collection(
+            name=collection_name,
+        )
+
+
+    def organize_document_chunks(
+        self,
+        document_chunks: List[Dict[str,Any]]
+    ) -> Tuple[List[str], List[str], List[Metadata]]:
+        ids = []
+        documents = []
+        metadatas = []
+        for chunk in document_chunks:
+            ids.append(chunk["metadata"]["chunk_id"])
+            documents.append(chunk["text"])
+            metadatas.append(chunk["metadata"])
+        return (ids, documents, metadatas)
+
+
+    def add_document_chunks(
+        self,
+        collection_name: str,
+        embedding_function: EmbeddingFunction[Embeddable],
+        document_chunks: List[Dict[str,Any]]
+    ) -> Collection:
+        # get collection
+        collection = self.get_or_create_collection(collection_name, embedding_function)
+        # map chunks from DocumentProcessor to required sublists
+        (ids, documents, metadatas) = self.organize_document_chunks(document_chunks)
+        # load into database collection
+        collection.add(
+            ids=ids,
+            documents=documents,
+            metadatas=metadatas,
+        )
+
+        return collection
--- tests/test_rag_utils.py
+++ tests/test_rag_utils.py
@@ -1,16 +1,66 @@
 import pytest
 import os
 
-from rag_utils import DocumentProcessor
+from rag_utils import (
+    DocumentProcessor, 
+    ChromaInterface,
+)
+
     
-def test_single_document():
+# def test_single_document():
+#     current_dir = os.path.dirname(__file__)
+#     test_document = os.path.join(current_dir, "resources", "FAA_Order_8040.1C.pdf")
+#     output_dir = os.path.join(current_dir, "output")
+    
+#     # create document processor and process test document
+#     processor = DocumentProcessor(
+#         document_directory_path=None, 
+#         output_path=output_dir
+#     )
+#     chunks = processor.process_single_document(
+#         document_path=test_document
+#     )
+#     print(f"Length of chunks {len(chunks)}")
+
+#     assert(len(chunks) == 9)
+#     assert(chunks[8]["text"] == "FAA Form 1320-19 (8-89)(Representation)")
+#     assert(chunks[8]["metadata"]["file_directory"] == "/hawes/projects/AVIATION_NLP/fy25/users/chen/repos/gitlab-alue/alue/tests/resources")
+#     assert(chunks[8]["metadata"]["filename"] == "FAA_Order_8040.1C.pdf")
+#     assert(chunks[8]["metadata"]["page_number"] == 4)
+#     assert(chunks[8]["metadata"]["chunk_id"] == "FAA_Order_8040.1C-8")
+
+
+def test_database_creation():
     current_dir = os.path.dirname(__file__)
     test_document = os.path.join(current_dir, "resources", "FAA_Order_8040.1C.pdf")
     output_dir = os.path.join(current_dir, "output")
     
-    processor = DocumentProcessor(None, output_dir)
-    
-    chunks = processor.process_single_document(test_document)
-    print(f"Length of chunks {len(chunks)}")
+    # create document chunks
+    processor = DocumentProcessor(
+        document_directory_path=None, 
+        output_path=output_dir
+    )
+    chunks = processor.process_single_document(
+        document_path=test_document
+    )
 
-    assert(len(chunks) > 0)
+    # write to database
+    chroma_interface = ChromaInterface(
+        database_path=os.path.join(output_dir, "chromadb")
+    )
+    chroma_interface.add_document_chunks(
+        collection_name="test_collection", 
+        embedding_function=ChromaInterface.get_local_embedding_function(),
+        document_chunks=chunks
+    )
+
+    # read from database
+    collection = chroma_interface.get_collection(collection_name="test_collection")
+    getResult = collection.get(ids=["FAA_Order_8040.1C-8"])
+    queryResult = collection.query(query_texts=["disposition of records"], n_results=1)
+    # assert presence of collection and contents
+    assert(collection.count() == 9)
+    assert(getResult["documents"][0] == "FAA Form 1320-19 (8-89)(Representation)")
+    assert(queryResult["ids"][0][0] == "FAA_Order_8040.1C-6")
+
+
